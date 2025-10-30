65c4c5a189ae9456bbd56f690dd3c24da15eaeac
Shureck <Sashalev200149@gmail.com>
2025-10-30 12:23:11 +0300

    first commit

--- src/connect.cpp
+++ src/connect.cpp
@@ -1,19 +1,19 @@
-// #include <WiFi.h>
-// const char* ssid = "Samsung_IoT";
-// const char* password = "IOT5iot5";
-// void setup() {
-//   Serial.begin(9600);
-//   WiFi.begin(ssid, password);
+#include <WiFi.h>
+const char* ssid = "Samsung_IoT";
+const char* password = "IOT5iot5";
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
 
-//   Serial.print("Connecting to WiFi");
-//   while (WiFi.status() != WL_CONNECTED) {
-//     delay(500);
-//     Serial.print(".");
-//   }
-//   Serial.println("\nConnected!");
-//   Serial.print("IP Address: ");
-//   Serial.println(WiFi.localIP());
-// }
-// void loop() {
-//   // Основной цикл
-// }
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+void loop() {
+  // Основной цикл
+}
